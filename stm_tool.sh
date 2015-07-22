fcdac981c5c5e7ac2dad2c7e4d0344461a2d5e31
kunulee <kunukunu@naver.com>
2015-07-21 19:52:52 -0700

    before seq_num ver

---
+++ stm_tool.sh
@@ -0,0 +1,15 @@
+make clean 
+make
+#cp ./include/stm.h /home/kunulee/HEAPO/HEAPO_STM_LIB/KV_tiny/stm_lib/
+#cp ./include/mod_ab.h /home/kunulee/HEAPO/HEAPO_STM_LIB/KV_tiny/stm_lib/
+#cp ./src/stm_internal.h /home/kunulee/HEAPO/HEAPO_STM_LIB/KV_tiny/stm_lib/
+#cp ./lib/libstm.a /home/kunulee/HEAPO/HEAPO_STM_LIB/KV_tiny/
+
+cp ./lib/libstm.a /home/kunulee/HEAPO/HEAPO_STM_LIB/Library/
+cp ./include/stm.h /home/kunulee/HEAPO/HEAPO_STM_LIB/Library/
+cp ./include/mod_ab.h /home/kunulee/HEAPO/HEAPO_STM_LIB/Library/
+cp ./src/stm_internal.h /home/kunulee/HEAPO/HEAPO_STM_LIB/Library/
+
+#cp ./include/stm.h /home/kunulee/HEAPO/HEAPO_STM_LIB/KV_tiny/list/ 
+#cp ./include/mod_ab.h /home/kunulee/HEAPO/HEAPO_STM_LIB/KV_tiny/list/
+#cp ./lib/libstm.a /home/kunulee/HEAPO/HEAPO_STM_LIB/KV_tiny/list/
